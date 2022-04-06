8f94504822c12c4accaa48c5587712c12dc71275
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2022-04-06 20:23:39 +0200

    add home link to navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
 function App() {
   return (
-    <div className="App">
-     <Navbar />
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={ <Home /> } />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
-import Logo from '../assets/pizzaLogo.png'
+import Logo from '../assets/pizzaLogo.png';
+import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className='navar'>
-        <div className='leftside'>
-            <img src={ Logo } />
-        </div>
-        <div className='rightside'></div>
+      <div className='leftside'>
+        <img src={Logo} />
+      </div>
+      <div className='rightside'>
+          <Link to='/'>Home</Link>
+      </div>
     </div>
   );
 };

34fb5667db9f67ae63636fe54d48db3c7d36518b
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2022-04-07 17:00:44 +0200

    Add links navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,18 @@
 import React from 'react';
 import Logo from '../assets/pizzaLogo.png';
 import { Link } from 'react-router-dom';
+import '../styles/Navbar.css'
 const Navbar = () => {
   return (
-    <div className='navar'>
-      <div className='leftside'>
+    <div className='navbar'>
+      <div className='leftSide'>
         <img src={Logo} />
       </div>
-      <div className='rightside'>
-          <Link to='/'>Home</Link>
+      <div className='rightSide'>
+        <Link to='/'>Home</Link>
+        <Link to='/menu'>Menu</Link>
+        <Link to='/abbout'>About</Link>
+        <Link to='/contact'>Contact</Link>
       </div>
     </div>
   );

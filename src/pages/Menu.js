54cf89992a3360e485af6ee9b8d0dd83e5f15e24
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2022-04-08 18:13:26 +0200

    add menuList

---
+++ src/pages/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Menu = () => {
+  return (
+    <div className='menu'>
+      <h1 className='menuTitle'>Our menu</h1>
+      <div className='menuList'></div>
+    </div>
+  );
+};
+
+export default Menu;
